0bc65f530319ab5f92c54a22e99a62385fe19466
Eleazar Udo <elzoremmanuel@gmail.com>
2022-05-15 15:40:10 +0300

    Complete with CR functionality on server

---
+++ server/my-app/README.md
@@ -0,0 +1,2 @@
+This contains the server and database for the app.
+Currently working on CRUD capabilities.
---
+++ server/my-app/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors()); //This permits get requests from React
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //This allows getting axios request body from react
+
+
+mongoose.connect('mongodb://localhost:27017/projectDB');
+
+const PORT = process.env.PORT;
+
+const Schema = mongoose.Schema;
+const projectSchema = new Schema({
+    pName: String,
+    Summary: String,
+    Url: String
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+app.route("/")
+    .get((req, res) => {
+        res.send();
+    })
+
+
+app.route("/projects") //This sends all the projects in the database to the client
+    .get((req, res) => {
+        Project.find({}, (result, error) => {
+            if (error) {
+                console.log(error);
+                res.send(error);
+            } else {
+                res.send(result);
+            }
+        })
+    })
+    .post((req, res) => {
+        const result = req.body
+        console.log(result);
+        Project.create({
+            pName: result.pName,
+            Summary: result.summary,
+            Url: result.url
+        });
+        res.redirect("/projects");
+    });
+
+
+
+
+app.listen(PORT, () => {
+    console.log(PORT || 3000);
+});
